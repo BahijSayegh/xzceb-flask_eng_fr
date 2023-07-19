70040164d6fd1328bc6d7b39d3158645ab7ca101
BahijSayegh <info@bahijsayegh.de>
2023-07-18 21:59:38 -0400

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -15,10 +15,12 @@ class TestTranslator(unittest.TestCase):
 
     def test_hello_english_to_french(self):
         self.assertEqual("bonjour", english_to_french('hello'))
+        self.assertNotEqual("bonsoir", english_to_french('hello'))
 
 
     def test_bonjour_french_to_english(self):
         self.assertEqual('hello', french_to_english('bonjour'))
+        self.assertNotEqual("hello", english_to_french('bonsoir'))
 
 
 if __name__ == '__main__':
